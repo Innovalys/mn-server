66ffd0e4c6b8deb10c480b59edf056e7a00d9020
vuzi <vuzi@free.fr>
2015-04-10 17:00:19 +0200

    Small doc change

--- src/MangaNetwork/renderer.php
+++ src/MangaNetwork/renderer.php
@@ -5,10 +5,12 @@
  */
 
 /**
- * Renderer used to write the response in a formated way
+ * Renderer used to write the response in a formated way. The json_encode method
+ * will by itself walk throw every elements of objets, so objects along with
+ * arrays or natural values can be sended
  *
  * @param mixed $data The data to display. This data will be converted to JSON using
- * PHP's json_encode method, so associatives arrays will often be the best choice
+ * PHP's json_encode method
  * @param bool $error True if an error occurred, false otherwise
  * @param int $code The HTTP code of the request
  */
