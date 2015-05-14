21d7b8df2f7c80d6843d31196f2304e80f78baa9
izyj <iisiramen@gmail.com>
2015-05-14 18:48:00 +0200

    commentaires

--- src/MangaNetwork/action/user/search_personnal_manga.php
+++ src/MangaNetwork/action/user/search_personnal_manga.php
@@ -17,7 +17,7 @@
 	
 	$db = GetDBConnection();
 
-		// Get manga_id
+		// recupere les id des mangas
 		$query = $db->prepare("SELECT manga_id FROM user_has_manga 
 							   WHERE user_id = :id  ");
 		$query->execute($user_info);
@@ -26,7 +26,7 @@
 		if(!$data)
 			return false;
 
-		// Get manga data
+		// recupere les données des mangas
 		foreach($data['manga_id'] as $manga){
 		
 			
@@ -38,13 +38,13 @@
 			if(!$data){return false;}
 				
 			
-			// Get genre
+			// recupere genre
 			$query = $db->prepare("SELECT genre.name FROM genre JOIN genre_has_manga
 								   WHERE genre_has_manga.manga_id = ? AND genre_has_manga.genre_id = genre.id");
 			$query->execute([$data2['id']]);
 			$data['genres'] = $query->fetchAll(PDO::FETCH_COLUMN, 0);
 
-			// Get authors
+			// recupere auteurs
 			$query = $db->prepare("SELECT author.name FROM author JOIN author_has_manga
 								   WHERE author_has_manga.manga_id = ? AND author_has_manga.author_id = author.id");
 			$query->execute([$data['id']]);
