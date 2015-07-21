59bd6255ef0303c9878cd5987a502c0d0252ec4d
Vuzi <vuzi@free.fr>
2015-07-21 10:28:41 +0200

    User following and followed are now returned in the user endpoint

--- src/MangaNetwork/action/user/get.php
+++ src/MangaNetwork/action/user/get.php
@@ -13,8 +13,34 @@ function GetUser($context) {
 							FROM user
 							WHERE id = ?");
 
-	$response = $query->execute([$data]);
+	$query->execute([$data]);
 	$response = $query->fetch(PDO::FETCH_ASSOC);
+
+	// Users followed
+	$query = $db->prepare("SELECT id, login, mail, name, credentials
+							FROM user
+							INNER JOIN user_has_user
+							ON user.id = user_has_user.user_id_followed AND user_has_user.user_id_following = ?");
+
+	$query->execute([$data]);
+
+	$response["followed"] = [];
+	while($followed = $query->fetch(PDO::FETCH_ASSOC)) {
+		$response["followed"][] = $followed;
+	}
+
+	// Users following
+	$query = $db->prepare("SELECT id, login, mail, name, credentials
+							FROM user
+							INNER JOIN user_has_user
+							ON user.id = user_has_user.user_id_following AND user_has_user.user_id_followed = ?");
+
+	$query->execute([$data]);
+
+	$response["following"] = [];
+	while($following = $query->fetch(PDO::FETCH_ASSOC)) {
+		$response["following"][] = $following;
+	}
 
 	return $response;
 }
